6555693967fa74f981e6e819ed09d4bcb87ee2d7
devmaxwel <eng.maxwel.ochieng@gmail.com>
2021-12-11 13:36:02 +0300

    Pop-up first commit

---
+++ src/Components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './todo.module.css';
+
+function Todo() {
+    return (
+        <div className={classes.card}>
+            <h1>Todo App</h1>
+            <h4>Learn React</h4>
+        </div>
+    )
+}
+
+export default Todo;
